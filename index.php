9845fac0c4acefdba5408935fb46055fed1ff813
Amedeix <amedeo.laudisio@gmail.com>
2022-11-09 10:44:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
 echo "Ciao :) </br> Ecco tutte le info. </br> Buona fortuna.";
-
+echo "<table>";
 foreach ($_SERVER as $key=>$item){
-    echo "<b>$key</b> => $item <br>";
+	echo "<tr>";
+    echo "<td><b>$key</b></td><td>$item </td>";
+	echo "</tr>";
 }
+echo "</table>";
 ?>
